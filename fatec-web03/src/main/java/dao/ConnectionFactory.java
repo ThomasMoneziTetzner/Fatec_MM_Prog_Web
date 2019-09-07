fe62e3c26d1127af09cb385e41cab7dd7289b2bc
Rodrigo Henrique Martins <rodrimartins2005@gmail.com>
2019-09-07 16:57:26 -0300

    Inclusão do Projeto WEB03

---
+++ fatec-web03/src/main/java/dao/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection getConnection() throws ClassNotFoundException {
+        final Connection conex;
+        Class.forName("com.mysql.jdbc.Driver");
+        try {
+            conex = DriverManager.getConnection("jdbc:mysql://localhost/time","root",
+                    "root");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conex;
+    }
+}
+
